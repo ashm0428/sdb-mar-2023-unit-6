1d38fd0bdace892eba2a873cb4ae3d80b81e777c
Ashlee Lindstrom <ashm0428@gmail.com>
2023-05-06 17:51:45 -0600

    commiting minor changes

--- lessons/driver-log/app.js
+++ lessons/driver-log/app.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 const userController = require("./controllers/user.controller");
 const driverlogController = require("./controllers/driverlog.controller")
 
@@ -11,6 +12,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/driverlog-db");
 const db = mongoose.connection
 
 db.once("open", ()=> console.log("Connected to the DB"));
+app.use(cors());
 app.use(express.json());
 app.use("/user", userController);
 app.use("/log", driverlogController);
